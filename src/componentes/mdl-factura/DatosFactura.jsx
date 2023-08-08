6d52a1213e539f3540afaf62621968ad91f616b0
Byron Gonzalez <ing.byrongonzalez@gmail.com>
2023-08-08 16:11:25 -0600

    src/componentes/mdl-factura/DatosFactura.jsx: Componente DatosFactura.jsx creado

---
+++ src/componentes/mdl-factura/DatosFactura.jsx
@@ -0,0 +1,8 @@
+export const DatosFactura = () => {
+    return (<>
+        <ul className="list-group mt-3">
+            <li className="list-group-item"><span className="fw-bold">Factura #:</span> 1</li>
+            <li className="list-group-item"><span className="fw-bold">Tipo de factura:</span> Contado, credito, etc</li>
+        </ul>
+    </>);
+}
