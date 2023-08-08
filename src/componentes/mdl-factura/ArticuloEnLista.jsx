e141349bcab1dd62dd956c42a9dc1b2b6cb97a94
Byron Gonzalez <ing.byrongonzalez@gmail.com>
2023-08-08 16:23:07 -0600

    src/componentes/mdl-factura/ArticuloEnLista.jsx: componente ArticuloEnLista.jsx creado

---
+++ src/componentes/mdl-factura/ArticuloEnLista.jsx
@@ -0,0 +1,25 @@
+export const ArticuloEnLista = () => {
+    return (<>
+        <tr>
+            <td>Botones</td>
+            <td>Producto de prueba</td>
+            <td>1</td>
+            <td>500</td>
+            <td>500</td>
+        </tr>
+        <tr>
+            <td>Botones</td>
+            <td>Producto de prueba</td>
+            <td>2</td>
+            <td>100</td>
+            <td>200</td>
+        </tr>
+        <tr>
+            <td>Botones</td>
+            <td>Producto de prueba</td>
+            <td>3</td>
+            <td>150</td>
+            <td>450</td>
+        </tr>
+    </>);
+}
