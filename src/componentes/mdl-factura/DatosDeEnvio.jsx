246aeb6af1849234a63b501636dad6e2e5145f1c
Byron Gonzalez <ing.byrongonzalez@gmail.com>
2023-08-08 16:15:33 -0600

    src/componentes/mdl-factura/DatosDeEnvio.jsx: Componente DatosDeEnvio.jsx creado

---
+++ src/componentes/mdl-factura/DatosDeEnvio.jsx
@@ -0,0 +1,9 @@
+export const DatosDeEnvio = () => {
+    return (<>
+        <ul className="list-group">
+            <li className="list-group-item fw-bold fs-6 active">Datos de la empresa</li>
+            <li className="list-group-item">Direccion</li>
+            <li className="list-group-item">Numero de calle</li>
+        </ul>
+    </>);
+}
