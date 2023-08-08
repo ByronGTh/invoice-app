0e42a90b09eafecebce43aae63d470630cd6da7a
Byron Gonzalez <ing.byrongonzalez@gmail.com>
2023-08-08 16:13:46 -0600

    src/componentes/mdl-factura/DatosDeCliente.jsx: Componenete DatosDeCliente.jsx creado

---
+++ src/componentes/mdl-factura/DatosDeCliente.jsx
@@ -0,0 +1,10 @@
+export const DatosDeCliente = () => {
+    return (<>
+        <ul className="list-group">
+            <li className="list-group-item fw-bold fs-6 active">Datos de cliente</li>
+            <li className="list-group-item">Nombre Apellido</li>
+            <li className="list-group-item">Pais y ciudad</li>
+            <li className="list-group-item">Calle y numero de calle</li>
+        </ul>
+    </>);
+}
