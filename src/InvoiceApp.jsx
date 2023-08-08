a81d581cb635807ac72c9464e1f46197aac57afc
Byron Gonzalez <ing.byrongonzalez@gmail.com>
2023-08-08 16:28:32 -0600

    src/InvoiceApp.jsx: componentes DatosDeFactura, DatosDeCliente, DatosDeEnvio, ListaDeFactura y ArticuloEnLista incluidos en InvoiceApp.jsx

--- src/InvoiceApp.jsx
+++ src/InvoiceApp.jsx
@@ -1,3 +1,8 @@
+import { DatosDeCliente } from "./componentes/mdl-factura/DatosDeCliente";
+import { DatosDeEnvio } from "./componentes/mdl-factura/DatosDeEnvio";
+import { DatosFactura } from "./componentes/mdl-factura/DatosFactura";
+import { ListaDeFactura } from "./componentes/mdl-factura/ListaDeFactura";
+
 export const InvoiceApp = () => {
   return (<>
     <div className="container-fluid">
@@ -19,27 +24,15 @@ export const InvoiceApp = () => {
                 </li>
               </ul>
               <div class="tab-content" id="myTabContent">
-
+                <DatosFactura/>
                 <div class="tab-pane fade show active" id="home-tab-pane" role="tabpanel" aria-labelledby="home-tab" tabindex="0">
-                  <ul className="list-group mt-3">
-                    <li className="list-group-item"><span className="fw-bold">ID:</span> 1</li>
-                    <li className="list-group-item"><span className="fw-bold">Name:</span> elementos de prueba</li>
-                  </ul>
+                  
                   <div className="row mt-3">
                     <div className="col-6">
-                      <ul className="list-group">
-                        <li className="list-group-item fw-bold fs-6 active">Datos de cliente</li>
-                        <li className="list-group-item">Nombre Apellido</li>
-                        <li className="list-group-item">Pais y ciudad</li>
-                        <li className="list-group-item">Calle y numero de calle</li>
-                      </ul>
+                      <DatosDeCliente/>
                     </div>
                     <div className="col-6">
-                      <ul className="list-group">
-                        <li className="list-group-item fw-bold fs-6 active">Datos de la empresa</li>
-                        <li className="list-group-item">Direccion</li>
-                        <li className="list-group-item">Numero de calle</li>
-                      </ul>
+                      <DatosDeEnvio/>
                     </div>
                   </div> {/* Fin de row informativo */}
 
@@ -52,112 +45,7 @@ export const InvoiceApp = () => {
               <div className="row mt-3">
                 <div className="col">
                   {/* <h3>Productos en factura</h3> */}
-                  <table className="table table-striped table-hover caption-top">
-                    <caption className="fw-bold fs-5">Factura</caption>
-                    <thead className="table-group-divider">
-                      <tr>
-                      <th>Acciones</th>
-                        <th>Producto</th>
-                        <th>Cantidad</th>
-                        <th>Precio</th>
-                        <th>Subtotal</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
-                        <td>Botones</td>
-                        <td>Producto de prueba</td>
-                        <td>1</td>
-                        <td>500</td>
-                        <td>500</td>
-                      </tr>
-                      <tr>
-                        <td>Botones</td>
-                        <td>Producto de prueba</td>
-                        <td>2</td>
-                        <td>100</td>
-                        <td>200</td>
-                      </tr>
-                      <tr>
-                        <td>Botones</td>
-                        <td>Producto de prueba</td>
-                        <td>3</td>
-                        <td>150</td>
-                        <td>450</td>
-                      </tr>
-                    </tbody>
-                    <tfoot className="table-active table-group-divider">
-                      <tr>
-                        <td rowspan="8">
-                          <div class="mb-3">
-                            <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
-                            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-                          </div>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td colSpan={3} className="text-end">subtotal:</td>
-                        <td><span className="badge bg-primary">750</span></td>
-                      </tr>
-                      <tr>
-                        <td colSpan={3} className="text-end">impuesto (15%):</td>
-                        <td><span className="badge bg-primary">750</span></td>
-                      </tr>
-                      <tr>
-                        <td colSpan={3} className="text-end">descuento:</td>
-                        <td><span className="badge bg-primary">750</span></td>
-                      </tr>
-                      <tr>
-                        <td colSpan={3} className="text-end">excento:</td>
-                        <td><span className="badge bg-primary">750</span></td>
-                      </tr>
-                      <tr>
-                        <td colSpan={3} className="text-end">Envio:</td>
-                        <td><span className="badge bg-primary">50</span></td>
-                      </tr>
-                      <tr>
-                        <td colSpan={3} className="text-end">total articulos:</td>
-                        <td><span className="badge bg-primary">750</span></td>
-                      </tr>
-                      <tr>
-                        <td colSpan={3} className="text-end">Total:</td>
-                        <td><span className="badge bg-primary">750</span></td>
-                      </tr>
-                      {/* <tr>
-                        <td>
-                          <table className="table table-striped table-hover table-bordered">
-                            <tbody>
-                            <tr>
-                              <td className="text-end">subtotal:</td>
-                              <td><span class="badge bg-primary">750</span></td>
-                            </tr>
-                            <tr>
-                              <td className="text-end">impuesto (15%):</td>
-                              <td><span class="badge bg-primary">750</span></td>
-                            </tr>
-                            <tr>
-                              <td className="text-end">descuento:</td>
-                              <td><span class="badge bg-primary">750</span></td>
-                            </tr>
-                            <tr>
-                              <td className="text-end">excento:</td>
-                              <td><span class="badge bg-primary">750</span></td>
-                            </tr>
-                            <tr>
-                              <td className="text-end">total articulos:</td>
-                              <td><span class="badge bg-primary">750</span></td>
-                            </tr>
-                            <tr>
-                              <td className="text-end">Total:</td>
-                              <td><span class="badge bg-primary">750</span></td>
-                            </tr>
-
-                            </tbody>
-                          </table>
-                        </td>
-                      </tr> */}
-                    </tfoot>
-                  </table>
+                  <ListaDeFactura/>
                 </div>
               </div> {/* Fin del row contenedor de la tabla */}
               {/* <div className="row">
