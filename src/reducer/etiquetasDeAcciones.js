05784281313d817ad97f9c71a3e1bd923fd57b58
Byron Gonzalez <ing.byrongonzalez@gmail.com>
2023-08-17 17:50:02 -0600

    src/reducer/etiquetasDeAcciones.js: archivo creado con las variables contantes para implementar en el reducer

---
+++ src/reducer/etiquetasDeAcciones.js
@@ -0,0 +1,3 @@
+export const AGREGAR_PRODUCTO_A_FACTURA = 'AgregarProductoAFactura';
+export const ACTUALIZAR_CANTIDAD_DE_PRODUCTO = 'ActuralizarCantidadDeProducto';
+export const ELIMINAR_PRODUCTO_DE_FACTURA = 'EliminarProductoDeFactura';
