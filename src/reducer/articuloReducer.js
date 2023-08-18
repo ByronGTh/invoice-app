e76ecc790b78c893d90034515d4d5c8ae0d3c3ef
Byron Gonzalez <ing.byrongonzalez@gmail.com>
2023-08-17 18:20:17 -0600

    src/reducer/articuloReducer.js: implementando variables constantes del archivo etiquetasDeAcciones.js

--- src/reducer/articuloReducer.js
+++ src/reducer/articuloReducer.js
@@ -1,6 +1,8 @@
+import { ACTUALIZAR_CANTIDAD_DE_PRODUCTO, AGREGAR_PRODUCTO_A_FACTURA, ELIMINAR_PRODUCTO_DE_FACTURA } from "./etiquetasDeAcciones";
+
 export const articuloReducer = (state = [], action) => {
     switch (action.type) {
-        case 'AgregarProductoAFactura':
+        case AGREGAR_PRODUCTO_A_FACTURA:
             
             return [
                 ...state,
@@ -9,7 +11,7 @@ export const articuloReducer = (state = [], action) => {
                   cantidad: 1,
                 }
               ];
-        case 'ActuralizarCantidadDeProducto':
+        case ACTUALIZAR_CANTIDAD_DE_PRODUCTO:
             
             return state.map( (i) => {
                 if( i.producto.id === action.payload.id ){
@@ -22,7 +24,7 @@ export const articuloReducer = (state = [], action) => {
                 }
                 return i;
               });
-        case 'EliminarProductoDeFactura':
+        case ELIMINAR_PRODUCTO_DE_FACTURA:
             
             return state.filter( (i) => i.producto.id !== action.payload );
     
