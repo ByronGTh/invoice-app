04e4e84056deaa9943319a222aa34d8e4f2dc6d1
Byron Gonzalez <ing.byrongonzalez@gmail.com>
2023-08-18 20:17:30 -0600

    src/main.jsx: Implementado el componeten BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { InvoiceApp } from './InvoiceApp'
 import { ListadoDeProductos } from './componentes/mdl-producto/ListadoDeProductos'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <InvoiceApp />
-  </React.StrictMode>,
+    <BrowserRouter>
+      <InvoiceApp />
+    </BrowserRouter>
+  </React.StrictMode>
 )
