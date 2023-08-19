79176d48211c6b602bce2b1f7f474ddba2a8558b
Byron Gonzalez <ing.byrongonzalez@gmail.com>
2023-08-18 20:26:25 -0600

    src/InvoiceApp.jsx: Definiendo esquema de rutas de la aplicacion por medio de los componentes Routes y Route

--- src/InvoiceApp.jsx
+++ src/InvoiceApp.jsx
@@ -1,3 +1,4 @@
+import { Navigate, Route, Routes } from "react-router-dom";
 import { DatosDeCliente } from "./componentes/mdl-factura/DatosDeCliente";
 import { DatosDeEnvio } from "./componentes/mdl-factura/DatosDeEnvio";
 import { DatosFactura } from "./componentes/mdl-factura/DatosFactura";
@@ -11,66 +12,73 @@ export const InvoiceApp = () => {
   
   return (<>
     <div className="container-fluid">
-      <div className="row">
-        <div className="col">
-          <div className="card">
-            <div className="card-header">
-              Ejemplo de factura
-            </div>
-            <div className="card-body">
-
-
-              <ul className="nav nav-tabs" id="myTab" role="tablist">
-                <li className="nav-item" role="presentation">
-                  <button className="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home-tab-pane" type="button" role="tab" aria-controls="home-tab-pane" aria-selected="true">Factura</button>
-                </li>
-                <li className="nav-item" role="presentation">
-                  <button className="nav-link" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile-tab-pane" type="button" role="tab" aria-controls="profile-tab-pane" aria-selected="false">Productos</button>
-                </li>
-              </ul>
-              <div className="tab-content" id="myTabContent">
-                
-                <div className="tab-pane fade show active" id="home-tab-pane" role="tabpanel" aria-labelledby="home-tab" tabIndex="0">
-                  <div className="row">
-                    <div className="col">
-                      <DatosFactura/>
+      <Routes>
+        <Route path="factura" element={
+          <div className="row">
+            <div className="col">
+              <div className="card">
+                <div className="card-header">
+                  Ejemplo de factura
+                </div>
+                <div className="card-body">
+    
+    
+                  <ul className="nav nav-tabs" id="myTab" role="tablist">
+                    <li className="nav-item" role="presentation">
+                      <button className="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home-tab-pane" type="button" role="tab" aria-controls="home-tab-pane" aria-selected="true">Factura</button>
+                    </li>
+                    <li className="nav-item" role="presentation">
+                      <button className="nav-link" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile-tab-pane" type="button" role="tab" aria-controls="profile-tab-pane" aria-selected="false">Productos</button>
+                    </li>
+                  </ul>
+                  <div className="tab-content" id="myTabContent">
+                    
+                    <div className="tab-pane fade show active" id="home-tab-pane" role="tabpanel" aria-labelledby="home-tab" tabIndex="0">
+                      <div className="row">
+                        <div className="col">
+                          <DatosFactura/>
+                        </div>
+                      </div>
+                      <div className="row mt-3">
+                        <div className="col-6">
+                          <DatosDeCliente/>
+                        </div>
+                        <div className="col-6">
+                          <DatosDeEnvio/>
+                        </div>
+                      </div> {/* Fin de row informativo */}
+    
+                    </div>
+    
+                    <div className="tab-pane fade" id="profile-tab-pane" role="tabpanel" aria-labelledby="profile-tab" tabIndex="0">
+                      <ListadoDeProductos manejadorDeProducto={agregarProductoAFactura}/>
                     </div>
                   </div>
+    
+    
                   <div className="row mt-3">
-                    <div className="col-6">
-                      <DatosDeCliente/>
+                    <div className="col">
+                      <ListaDeFactura  items={ productos } manejadorParaEliminar={manejadorEliminarProductoDeLista}/>
                     </div>
-                    <div className="col-6">
-                      <DatosDeEnvio/>
+                  </div> {/* Fin del row contenedor de la tabla */}
+                  <div className="row align-items-end">
+                    <div className="col">
+                      <form action="">
+                        <button type="submit" className="btn btn-primary">Facturar</button>
+                        <button type="button" className="btn btn-primary m-2">Cancelar factura</button>
+                      </form>
+    
                     </div>
-                  </div> {/* Fin de row informativo */}
-
-                </div>
-
-                <div className="tab-pane fade" id="profile-tab-pane" role="tabpanel" aria-labelledby="profile-tab" tabIndex="0">
-                  <ListadoDeProductos manejadorDeProducto={agregarProductoAFactura}/>
-                </div>
-              </div>
-
-
-              <div className="row mt-3">
-                <div className="col">
-                  <ListaDeFactura  items={ productos } manejadorParaEliminar={manejadorEliminarProductoDeLista}/>
-                </div>
-              </div> {/* Fin del row contenedor de la tabla */}
-              <div className="row align-items-end">
-                <div className="col">
-                  <form action="">
-                    <button type="submit" className="btn btn-primary">Facturar</button>
-                    <button type="button" className="btn btn-primary m-2">Cancelar factura</button>
-                  </form>
-
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-      </div>
+        }/>
+        <Route path="productos" element={ <ListadoDeProductos manejadorDeProducto={agregarProductoAFactura}/> }/>
+        <Route path="/" element={ <Navigate to={'/factura'}/> }/>        
+      </Routes>
+      
     </div>
   </>);
 }
