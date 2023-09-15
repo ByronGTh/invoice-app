fdd828a769343c0b232a629f3c3c59b98b493f15
Byron Gonzalez <ing.byrongonzalez@gmail.com>
2023-09-15 12:00:51 -0600

    src/services/productoService.js: funcion save agregada

--- src/services/productoService.js
+++ src/services/productoService.js
@@ -1,11 +1,26 @@
 import axios from "axios";
 import { products } from "../data/productos"
 
+const BASE_URL = 'http://localhost:8080/productos';
+
 export const getProductos = async() => {
     try {
-        const httpRequest = await axios.get('http://localhost:8080/productos');
+        const httpRequest = await axios.get(BASE_URL);
         return httpRequest;
     } catch (error) {
         console.error(error);
     }
+
+    return null;
 }
+
+export const save = async({}) => {
+    try {
+        return axios.post(BASE_URL, {name, description, price});
+    } catch (error) {
+        console.error(error);
+    }
+
+    return undefined;
+}
+
