ede9a2b4b59a96cd613e656d8b58c9abac12f7e6
Byron Gonzalez <ing.byrongonzalez@gmail.com>
2023-09-25 09:45:10 -0600

    src/componentes/navbar/Navbar.jsx: dropdown menu agregado e icono de usuario

--- src/componentes/navbar/Navbar.jsx
+++ src/componentes/navbar/Navbar.jsx
@@ -21,6 +21,16 @@ export const Navbar = () => {
                         </li>
                     </ul>
                 </div>
+                <div class="dropdown">
+  <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+  <i class="bi bi-person-fill"></i>
+  </button>
+  <ul class="dropdown-menu dropdown-menu-end">
+    <li><a class="dropdown-item" href="#">Action</a></li>
+    <li><a class="dropdown-item" href="#">Another action</a></li>
+    <li><a class="dropdown-item" href="#">Something else here</a></li>
+  </ul>
+</div>
             </div>
         </nav>
     </>);
