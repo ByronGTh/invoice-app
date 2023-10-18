d0f9d70cc528aa007644e42045dd94e21e45f032
Byron Gonzalez <ing.byrongonzalez@gmail.com>
2023-10-18 10:03:38 -0600

    src/componentes/mdl-usuario/ListaDeUsuarios.jsx: evaluando con operador ternario si hay objetos enviados, de no ser asi mostramos un alert con un menjase de advertencia

--- src/componentes/mdl-usuario/ListaDeUsuarios.jsx
+++ src/componentes/mdl-usuario/ListaDeUsuarios.jsx
@@ -32,34 +32,38 @@ export const ListaDeUsuarios = ()=>{
             <FormularioUsuario handlerAgregarUsuario={ handlerAgregarUsuario }/>
         </div>
         <div className="col-6">
-            <table className="table table-hover table-striped">
-                <thead>
-                    <tr>
-                        <th>Id</th>
-                        <th>Usuario</th>
-                        <th>Email</th>
-                        <th>Editar</th>
-                        <th>Eliminar</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {usuarios.map( user => (
-                        <tr key={user.id}>
-                            <td>{user.id}</td>
-                            <td>{user.nombre_usuario}</td>
-                            <td>{user.email}</td>
-                            <td>
-                                <button className="btn btn-secondary">Editar</button>
-                            </td>
-                            <td>
-                                <button className="btn btn-danger" onClick={()=> handlerEliminarUsuario(user.id)}>Eliminar</button>
-                            </td>
+            {usuarios.length === 0
+            ? <div className="alert alert-warning">No hay usuarios en el sistema</div>
+            : <table className="table table-hover table-striped">
+                    <thead>
+                        <tr>
+                            <th>Id</th>
+                            <th>Usuario</th>
+                            <th>Email</th>
+                            <th>Editar</th>
+                            <th>Eliminar</th>
                         </tr>
-                    )
-                    )}
-                    
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                        {usuarios.map( user => (
+                            <tr key={user.id}>
+                                <td>{user.id}</td>
+                                <td>{user.nombre_usuario}</td>
+                                <td>{user.email}</td>
+                                <td>
+                                    <button className="btn btn-secondary">Editar</button>
+                                </td>
+                                <td>
+                                    <button className="btn btn-danger" onClick={()=> handlerEliminarUsuario(user.id)}>Eliminar</button>
+                                </td>
+                            </tr>
+                        )
+                        )}
+                        
+                    </tbody>
+                </table>
+            }
+            
         </div>
     </div>
     );
