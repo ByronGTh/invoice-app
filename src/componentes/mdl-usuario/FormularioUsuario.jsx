a9c030a94923e8fec89bee7286662bb1dea4368b
Byron Gonzalez <ing.byrongonzalez@gmail.com>
2023-10-20 19:05:12 -0600

    src/componentes/mdl-usuario/FormularioUsuario.jsx: Realizando validaciones en el formulario, en el boton y en el input de la clave

--- src/componentes/mdl-usuario/FormularioUsuario.jsx
+++ src/componentes/mdl-usuario/FormularioUsuario.jsx
@@ -39,8 +39,10 @@ export const FormularioUsuario = ( {handlerAgregarUsuario, usuarioSeleccionado} 
     
     return(<form onSubmit={ onSubmit }>
         <input className="form-control my-3 w-75" type="text" name="nombre_usuario" onChange={ onInputChange } value={ nombre_usuario } placeholder="Nombre de usuario"/>
-        <input className="form-control my-3 w-75" type="password" name="clave" onChange={ onInputChange } value={ clave } placeholder="Clave"/>
+        {formularioUsuario.id > 0 || 
+            <input className="form-control my-3 w-75" type="password" name="clave" onChange={ onInputChange } value={ clave } placeholder="Clave"/>
+        }
         <input className="form-control my-3 w-75" type="email" name="email" onChange={ onInputChange } value={ email } placeholder="Email"/>
-        <button className="btn btn-success">Guardar</button>
+        <button className="btn btn-success">{formularioUsuario.id > 0 ? 'Editar' : 'Guardar'}</button>
     </form>);
 }
